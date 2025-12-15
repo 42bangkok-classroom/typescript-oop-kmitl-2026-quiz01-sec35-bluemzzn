750e9cdb3653f76303a88da7e3f63e9da84b0a52
Kuljira Namjai <68011621@kmitl.ac.th>
2025-12-15 10:01:01 +0700

    quiz6

---
+++ xo.ts
@@ -0,0 +1,17 @@
+let sizeXO = Number(process.argv[2]);
+
+if (Number.isNaN(sizeXO) || sizeXO <= 0 || sizeXO === undefined) {
+    process.stdout.write("Invalid Input");
+    process.exit();
+} else {
+    for (let i = 1; i <= sizeXO; i++) {
+        for (let j = 1; j <= sizeXO; j++) {
+            if (i === 1 || i === sizeXO || j === 1 || j === sizeXO) {
+                process.stdout.write("x");
+            } else {
+                process.stdout.write("o");
+            }
+        }
+        process.stdout.write("\n");
+    }
+}
